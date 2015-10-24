542ab7a12625b1735e6d496cde52dfc0ed20c44a
Bamboo <nmz89825@gmail.com>
2015-10-24 19:39:21 +0800

    Update combine.py

--- _src/om2py2w/2wex0/combine.py
+++ _src/om2py2w/2wex0/combine.py
@@ -1,32 +1,41 @@
 # -*- coding: utf-8 -*-         
 
+# -*- coding: utf-8 -*-         
+
 from Tkinter import *
-from time import localtime, strftime
 
 master = Tk()
-
-f = open('diary log.txt','a+')
+master.geometry("300x450+500+100")
+master.title("MyDiary Application")
 log = StringVar()
 newlog = StringVar()
 
 frame = Frame(master)
 frame.pack()
-		
-log.set(f.read())
-m = Message(master, textvariable=log)
-m.pack()
-		
-e = Entry(master, textvariable=newlog)	
+e = Entry(frame, textvariable=newlog,bg="green")
 
 def getnew(event):
 	line = newlog.get() # get input 
-	f = open('diary log.txt','a+')
-	edit_time = strftime("%a, %Y %b %d %H:%M:%S", localtime())
-	f.write('%s     %s\n' % (edit_time, line))
-	f.close()
-	e.delete(0,END) # delete input
+	#f = open('diary log.txt','a+')
+	#f.write('%s\n' % line)
+	#f.close()
+	#ml=Label(frame, text = line)
+	#ml.pack(anchor=W,fill=X,after=m)
+	
+	m = Message(frame, text=line)
+	m.pack(anchor=W)
+	
+	e.delete(0,END)
 
 e.bind("<Return>",getnew) # bind the keyboard ENTER as function getnew
-e.pack()	
+e.pack(anchor=W,fill=X)
+
+f = open('diary log.txt','a+')		
+log.set(f.read())
+m = Message(frame, textvariable=log)
+m.pack(anchor=W)
+	
+b = Button(master, text="QUIT", fg="red", command=frame.quit)
+b.pack(anchor=W,side = BOTTOM)
 
 mainloop()
