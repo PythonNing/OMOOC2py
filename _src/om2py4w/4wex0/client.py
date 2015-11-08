1d2e09c7cc5a35bd503af0bde60706fe876fed95
bambooom <nmz89825@gmail.com>
2015-11-09 01:04:52 +0800

    update client.py for CLI use

--- _src/om2py4w/4wex0/client.py
+++ _src/om2py4w/4wex0/client.py
@@ -3,61 +3,46 @@
 '''
 My Diary Web Application - CLI for client
 '''  
-#import socket
-#import sys
-#from bottle import request, route, template
-
-h = ['h','help','?']
-q = ['q','quit']
-r = ['r','sync']
-
+import urllib
 import urllib2
 import re
-content=urllib2.urlopen("http://localhost:8255/mydiary")
-print "http header:",content.info()
-print "http status:",content.getcode()
-print "url:",content.geturl()
-log = content.read()
-text = re.findall(r'<textarea .*>.*</textarea>', log, re.DOTALL)
-text2 = text[0]
-#tag = re.findall(r'<.*?>', text2, re.DOTALL)
-#print text2
-text3 = re.sub(r'<.*?>', '', text2)
-print text3
 
-#s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#server_address = ('localhost/mydiary', 8255)
-#s.connect(server_address)
+def get_log():
+	response = urllib2.urlopen("http://localhost:8255/mydiary")
+	html_code = response.read()
+	response.close()
+	text_area = re.findall(r'<textarea .*>.*</textarea>', html_code, re.DOTALL)
+	log = re.sub(r'<.*?>', '', text_area[0])
+	return log
 
-#message = "GET / HTTP/1.1\r\n\r\n"
+def write_log(message):
+	values = {'newdiary' : message}
+	data = urllib.urlencode(values)
+	req = urllib2.Request("http://localhost:8255/mydiary", data)
+	response = urllib2.urlopen(req)
 
-#s.sendall(message)
-
-#reply = s.recv(4096)
-
-#print reply
-
-#s.sendto('r', server_address) # ask server to send the latest log
-#log, server = s.recvfrom(4096)
-#print log
-
-#while True:
-#	message = raw_input(' Input here>') # ask for input
+def client():
+	h = ['h','help','?']
+	q = ['q','quit']
+	r = ['r','sync']
 	
-#	if message in h:
-#		print """ 
-#		Input h/help/? for help.
-#		Input q/quit to quit the process.
-#		Input r/sync to sync the diary log
-#		""" # print the help
-#	elif message in r: # for request, contact server to send back the log
-#		s.sendto(message, server_address) # client has to send the request for sync first
-#		log, server = s.recvfrom(4096) # so that client can get the reply
-#		print log
-#	elif message in q:
-#		print >>sys.stderr, 'Bye~'
-#		break
-#	else:
-#		s.sendto(message, server_address)
+	print get_log()
 
-s.close()
+	while True:
+		message = raw_input(' Input here>')
+		if message in h:
+			print """ 
+			Input h/help/? for help.
+			Input q/quit to quit the process.
+			Input r/sync to sync the diary log
+			"""
+		elif message in r:
+			print get_log()
+		elif message in q:
+			print 'Bye~'
+			break
+		else:
+			write_log(message)
+
+if __name__ == '__main__':
+	client()
