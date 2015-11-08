0ed577fdb37858ba4653416982e909d3ff87ad3f
bambooom <nmz89825@gmail.com>
2015-11-08 15:27:04 +0800

    simple web application done

--- _src/om2py4w/4wex0/main.py
+++ _src/om2py4w/4wex0/main.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# author: bambooom
 '''
 MyDiary Web Application
+Open web browser and access http://localhost:8255/mydiary
+You can read the old diary and input new diary
 '''
 
 from bottle import request, route, run, template
+from time import localtime, strftime
 
-def check_login(username, password):
-	if username == 'abc' and password == 'abc':
-		return True
-	else:
-		return False
+def read_diary():
+	f = open('diary log.txt','a+')
+	return f.read()
 
+def write_diary(newdiary):
+	f = open('diary log.txt','a+')
+	edit_time = strftime("%Y %b %d %H:%M:%S", localtime())
+	f.write('%s    %s\n' % (edit_time, newdiary))
+	f.close()
 
 @route('/mydiary')
 def start():
-	return template('diaryweb', diarylog='hello world')
+	log = read_diary()
+	return template("diaryweb", diarylog=log)
 
 @route('/mydiary', method='POST')
 def input_new():
 	newdiary = request.forms.get('newdiary')
-	return template('diaryweb', diarylog='hello world \n'+newdiary)
+	write_diary(newdiary)
+	log = read_diary()
+	return template("diaryweb", diarylog=log)
 
-#@route('/login', method='POST')
-#def do_login():
-#	username = request.forms.get('username')
-#	password = request.forms.get('password')
-#	if check_login(username, password):
-#		return "<p>Your login information was correct.</p>"
-#	else:
-#		return "<p>Login failed.</p>"
 
 if __name__ == '__main__':
-	run(host='localhost', port=8250, debug=True, reloader=True)
+	run(host='localhost', port=8255, debug=True, reloader=True)
 	
