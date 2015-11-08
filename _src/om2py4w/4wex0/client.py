5fc1571c6e90daf5409f14153145c04b0ea34be3
bambooom <nmz89825@gmail.com>
2015-11-08 23:30:07 +0800

    cli client test, try use urllib2

---
+++ _src/om2py4w/4wex0/client.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*- 
+# author: bambooom
+'''
+My Diary Web Application - CLI for client
+'''  
+#import socket
+#import sys
+#from bottle import request, route, template
+
+h = ['h','help','?']
+q = ['q','quit']
+r = ['r','sync']
+
+#coding=utf-8
+import urllib2
+import re
+content=urllib2.urlopen("http://localhost:8255/mydiary")
+print "http header:",content.info()
+print "http status:",content.getcode()
+print "url:",content.geturl()
+log = content.read()
+text = str(re.findall(r'<textarea .*>.*</textarea>', log, re.DOTALL))
+print text
+
+#s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#server_address = ('localhost/mydiary', 8255)
+#s.connect(server_address)
+
+#message = "GET / HTTP/1.1\r\n\r\n"
+
+#s.sendall(message)
+
+#reply = s.recv(4096)
+
+#print reply
+
+#s.sendto('r', server_address) # ask server to send the latest log
+#log, server = s.recvfrom(4096)
+#print log
+
+#while True:
+#	message = raw_input(' Input here>') # ask for input
+	
+#	if message in h:
+#		print """ 
+#		Input h/help/? for help.
+#		Input q/quit to quit the process.
+#		Input r/sync to sync the diary log
+#		""" # print the help
+#	elif message in r: # for request, contact server to send back the log
+#		s.sendto(message, server_address) # client has to send the request for sync first
+#		log, server = s.recvfrom(4096) # so that client can get the reply
+#		print log
+#	elif message in q:
+#		print >>sys.stderr, 'Bye~'
+#		break
+#	else:
+#		s.sendto(message, server_address)
+
+s.close()
