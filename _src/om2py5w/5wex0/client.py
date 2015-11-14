279dca9c32d1498a80253643a67ebff4313f408f
bambooom <nmz89825@gmail.com>
2015-11-14 16:33:22 +0800

    add list tag func

--- _src/om2py5w/5wex0/client.py
+++ _src/om2py5w/5wex0/client.py
@@ -9,11 +9,12 @@ sys.setdefaultencoding('utf-8')
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 HELP = '''
 Input h/help/? for help.
 Input q/quit to quit the process.
-Input r/sync to sync the diary log.
+Input s/sync to sync the diary log.
 Input lt/ListTags to list all tags.
 Input st:TAG to set or delete tags
 Input FLUSH to clear all diary entries.
@@ -25,7 +26,16 @@ def get_log():
 	tag = soup.textarea
 	return tag.string
 
-
+def get_tags():
+	diarylog = get_log()
+	tags_all = re.findall(r'TAG:.*?\n',diarylog,re.DOTALL)
+	tags = []
+	for i in tags_all:
+		temp = re.sub(r'TAG:','',i)
+		tags.append(re.sub(r'\n','',temp))
+	tags = list(set(tags))
+	for i in tags:
+		print i
 
 #	html_code = response.read()
 #	response.close()
@@ -44,28 +54,21 @@ def write_log(message, tags=''):
 #	response.close()
 
 def client():
-	h = ['h','help','?']
-	q = ['q','quit']
-	r = ['r','sync']
-	
-	get_log()
-	write_log("aaaa","bbbb")
 
-	#while True:
-	#	message = raw_input(' Input here>')
-	#	if message in h:
-	#		print """ 
-	#		Input h/help/? for help.
-	#		Input q/quit to quit the process.
-	#		Input r/sync to sync the diary log
-	#		"""
-	#	elif message in r:
-	#		print get_log()
-	#	elif message in q:
-	#		print 'Bye~'
-	#		break
-	#	else:
-	#		write_log(message)
+#	get_log()
+#	write_log("aaaa","bbbb")
+
+	while True:
+		message = raw_input(' Input here>')
+		if message in ['h','help','?']:
+			print HELP
+		elif message in ['s','sync']:
+			print get_log()
+		elif message in ['q','quit']:
+			print 'Bye~'
+			break
+		elif message in ['lt','ListTags']:
+			get_tags()
 
 
 if __name__ == '__main__':
